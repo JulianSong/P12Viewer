cf5c8f8320337691490ffdf5a4aec23b0c00acc1
Julian <geeksong@gmail.com>
2015-07-01 17:39:08 +0800

    edit file path

---
+++ P12ViewerTests/P12ViewerTests.m
@@ -0,0 +1,40 @@
+//
+//  P12ViewerTests.m
+//  P12ViewerTests
+//
+//  Created by imac-dev on 15/7/1.
+//  Copyright (c) 2015年 com.jullian. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface P12ViewerTests : XCTestCase
+
+@end
+
+@implementation P12ViewerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
